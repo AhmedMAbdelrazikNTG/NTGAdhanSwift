138ca3fe6754dff772ab5c79d8f7606f4f6c99b3
Amin <mahmoudamin373@gmail.com>
2025-09-11 00:27:20 +0300

    fix: calculation method

--- Example/Swift Example/Swift Example/ViewController.swift
+++ Example/Swift Example/Swift Example/ViewController.swift
@@ -16,13 +16,13 @@ class ViewController: UIViewController {
         
         let cal = Calendar(identifier: Calendar.Identifier.gregorian)
         let date = cal.dateComponents([.year, .month, .day], from: Date())
-        let coordinates = Coordinates(latitude: 35.78056, longitude: -78.6389)
-        var params = CalculationMethod.muslimWorldLeague.params
-        params.madhab = .hanafi
+        let coordinates = Coordinates(latitude: 29.986263, longitude: 31.26357)
+        var params = CalculationMethod.egyptian.params
+        params.madhab = .shafi
         if let prayers = PrayerTimes(coordinates: coordinates, date: date, calculationParameters: params) {
             let formatter = DateFormatter()
             formatter.timeStyle = .medium
-            formatter.timeZone = TimeZone(identifier: "America/New_York")!
+            formatter.timeZone = TimeZone.current
             
             NSLog("fajr %@", formatter.string(from: prayers.fajr))
             NSLog("sunrise %@", formatter.string(from: prayers.sunrise))
